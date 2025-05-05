cc3c1e8272df47881960bb1dcc0ea0030d533fde
Dmytro Turchynskyi <turchinskiyd@gmail.com>
2025-05-05 10:39:40 +0300

    add task init_db_table

--- airflow/dags/dag_nbu_to_db.py
+++ airflow/dags/dag_nbu_to_db.py
@@ -19,25 +19,25 @@ default_args = {
 }
 
 
-def save_to_postgres():
-    hook = PostgresHook(postgres_conn_id='nbu_postgres')
-    engine = hook.get_sqlalchemy_engine()
-
-    rates = fetch_nbu_exchange_rates()
-
-    with engine.begin() as conn:
-        for rate in rates:
-            insert_query = text("""
-                INSERT INTO exchange_rates (ccy, base_ccy, rate, date)
-                VALUES (:ccy, :base_ccy, :rate, :date)
-                ON CONFLICT (ccy, date) DO NOTHING
-            """)
-            conn.execute(insert_query, {
-                'ccy': rate['cc'],
-                'base_ccy': rate['base_ccy'],
-                'rate': rate['rate'],
-                'date': rate['exchangedate']
-            })
+# def save_to_postgres():
+#     hook = PostgresHook(postgres_conn_id='nbu_postgres')
+#     engine = hook.get_sqlalchemy_engine()
+#
+#     rates = fetch_nbu_exchange_rates()
+#
+#     with engine.begin() as conn:
+#         for rate in rates:
+#             insert_query = text("""
+#                 INSERT INTO exchange_rates (ccy, base_ccy, rate, date)
+#                 VALUES (:ccy, :base_ccy, :rate, :date)
+#                 ON CONFLICT (ccy, date) DO NOTHING
+#             """)
+#             conn.execute(insert_query, {
+#                 'ccy': rate['cc'],
+#                 'base_ccy': rate['base_ccy'],
+#                 'rate': rate['rate'],
+#                 'date': rate['exchangedate']
+#             })
 
 
 with DAG(
@@ -47,7 +47,9 @@ with DAG(
         schedule_interval='@daily',
         catchup=False,
         tags=['nbu', 'exchange', 'postgres']
-) as dag:
+        ) as dag:
+
+    # перевірка доступності API
     check_api = HttpSensor(
         task_id='check_nbu_api',
         http_conn_id='nbu_api',
@@ -59,9 +61,42 @@ with DAG(
         mode='poke'
     )
 
-    load_data = PythonOperator(
-        task_id='fetch_and_store_rates',
-        python_callable=save_to_postgres
+    # завантаження даних в XCom
+    def fetch_and_return_rates(**kwargs):
+        data = fetch_nbu_exchange_rates()
+        kwargs['ti'].xcom_push(key='exchange_data', value=data)
+
+    fetch_ndu_data = PythonOperator(
+        task_id = 'fetch_nbu_data',
+        python_callable = fetch_and_return_rates,
+        provide_context = True
     )
 
-    check_api >> load_data
+    def init_db():
+        hook = PostgresHook(postgres_conn_id = 'nbu_postgres')
+        create_query = """
+        CREATE TABLE IF NOT EXISTS exchange_rates (
+            r030 NUMERIC,
+            ccy VARCHAR(5),
+            txt TEXT,
+            base_ccy VARCHAR(3),
+            rate NUMERIC,
+            exchangedate DATE,
+            date TIMESTAMP DEFAULT now(),
+            PRIMARY KEY (ccy, exchangedate)
+        );
+        """
+        hook.run(create_query)
+
+    init_db_table = PythonOperator(
+        task_id = 'init_db_table',
+        python_callable=init_db
+    )
+
+
+    # load_data = PythonOperator(
+    #     task_id='fetch_and_store_rates',
+    #     python_callable=save_to_postgres
+    # )
+
+    check_api >> fetch_ndu_data >> init_db_table
