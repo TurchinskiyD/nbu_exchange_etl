ee75d198a6a89a0ea6d1d44521c554a82699ee55
Dmytro Turchynskyi <turchinskiyd@gmail.com>
2025-05-06 21:33:03 +0300

    change consumer and producer

--- kafka_module/consumer.py
+++ kafka_module/consumer.py
@@ -1,4 +1,6 @@
 from kafka import KafkaConsumer
+from kafka.errors import KafkaError
+from airflow.exceptions import AirflowSkipException
 import json
 import yaml
 
@@ -12,18 +14,25 @@ BOOTSTRAP_SERVERS = config["kafka"]["bootstrap_servers"]
 
 
 def read_from_kafka():
-    consumer = KafkaConsumer(
-        TOPIC,
-        bootstrap_servers=BOOTSTRAP_SERVERS,
-        value_deserializer=lambda v: json.loads(v.decode("utf-8")),
-        auto_offset_reset='earliest',
-        enable_auto_commit=False,
-        consumer_timeout_ms=10000,
-        group_id="airflow_consumer"
-    )
+    try:
+        consumer = KafkaConsumer(
+            TOPIC,
+            bootstrap_servers=BOOTSTRAP_SERVERS,
+            value_deserializer=lambda v: json.loads(v.decode("utf-8")),
+            auto_offset_reset='earliest',
+            enable_auto_commit=False,
+            consumer_timeout_ms=10000,
+            group_id="airflow_consumer"
+        )
 
-    data = [msg.value for msg in consumer]
+        data = [msg.value for msg in consumer]
 
-    consumer.close()
+        if not data:
+            raise AirflowSkipException('Нових повідомлень немає, зупиняємо Dag')
 
-    return data
+        consumer.commit()
+        consumer.close()
+
+        return data
+    except KafkaError as e:
+        raise Exception(f'KafkaError: {str(e)}')
--- kafka_module/producer.py
+++ kafka_module/producer.py
@@ -1,4 +1,6 @@
 from kafka import KafkaProducer
+from kafka.admin import KafkaAdminClient, NewTopic
+from kafka.errors import TopicAlreadyExistsError
 import json
 import yaml
 
@@ -7,9 +9,26 @@ with open('config/config.yaml') as f:
 
 TOPIC = config['kafka']['topic']
 BOOTSTRAP_SERVERS = config['kafka']['bootstrap_servers']
+NUM_PARTITIONS = 1  # змінити за потребою
+REPLICATION_FACTOR = 1
+
+
+def create_kafka_topic():
+    admin_client = KafkaAdminClient(bootstrap_servers=BOOTSTRAP_SERVERS)
+    topic = NewTopic(name=TOPIC, num_partitions=NUM_PARTITIONS, replication_factor=REPLICATION_FACTOR)
+
+    try:
+        admin_client.create_topics(new_topics=[topic], validate_only=False)
+        print(f"✅ Kafka topic '{TOPIC}' created with {NUM_PARTITIONS} partitions.")
+    except TopicAlreadyExistsError:
+        print(f"ℹ️ Kafka topic '{TOPIC}' already exists.")
+    finally:
+        admin_client.close()
 
 
 def publish_to_kafka(data: list):
+    create_kafka_topic()  # гарантує наявність партиційованого топіка
+
     producer = KafkaProducer(
         bootstrap_servers=BOOTSTRAP_SERVERS,
         value_serializer=lambda v: json.dumps(v).encode('utf-8')
